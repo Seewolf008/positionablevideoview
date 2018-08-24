7aa0d785f275d7ccc550993198598c006e087000
Seewolf008 <see.wolf008@gmail.com>
2018-08-25 00:29:34 +0200

    new Version with MediaControlls

--- app/src/main/java/com/example/sven/srbeastmodevideoview/MainActivity.java
+++ app/src/main/java/com/example/sven/srbeastmodevideoview/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.sven.srbeastmodevideoview;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.beastmodevideoview.SRBeastModeVideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SRBeastModeVideoView videoView = findViewById(R.id.surface);
+        Button button = findViewById(R.id.button);
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.hi);
+        videoView.initializeBeastMode(video);
+        //videoView.setDataSource(video);
+        //  preview.setSurfaceTextureListener(this);
+        button.setOnClickListener(v -> {
+            if (videoView.getMediaPlayer().isPlaying()) {
+                videoView.pause();
+            } else {
+                videoView.start();
+            }
+        });
     }
 }
--- beastmodevideoview/src/main/java/com/example/beastmodevideoview/SRBeastModeVideoView.java
+++ beastmodevideoview/src/main/java/com/example/beastmodevideoview/SRBeastModeVideoView.java
@@ -1,14 +1,21 @@
 package com.example.beastmodevideoview;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.PorterDuff;
 import android.graphics.SurfaceTexture;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
 import android.widget.SeekBar;
 
 import java.io.File;
@@ -16,13 +23,16 @@ import java.io.FileInputStream;
 
 import java.io.IOException;
 import java.util.logging.Handler;
+
 /**
  * Created by Sven on 19.08.2018.
  */
 
-public class SRBeastModeVideoView extends TextureView implements TextureView.SurfaceTextureListener{
+public class SRBeastModeVideoView extends TextureView implements TextureView.SurfaceTextureListener, MediaController.MediaPlayerControl {
 
     private MediaPlayer mediaPlayer;
+
+    private MediaController mediaController;
 
     private Uri video;
 
@@ -52,11 +62,16 @@ public class SRBeastModeVideoView extends TextureView implements TextureView.Sur
         Surface s = new Surface(surface);
 
         try {
-            //mediaPlayer = new MediaPlayer();
-            File file = new File(path);
-            FileInputStream inputStream = new FileInputStream(file);
-            mediaPlayer.setDataSource(inputStream.getFD());
-            inputStream.close();
+            if (path != null) {
+                if (!path.equals("")) {
+                    File file = new File(path);
+                    FileInputStream inputStream = new FileInputStream(file);
+                    mediaPlayer.setDataSource(inputStream.getFD());
+                    inputStream.close();
+                }
+            } else {
+                mediaPlayer.setDataSource(getContext(), video);
+            }
             mediaPlayer.setSurface(s);
             mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                 @Override
@@ -64,26 +79,20 @@ public class SRBeastModeVideoView extends TextureView implements TextureView.Sur
                     while (mediaPlayer.getVideoHeight() == 0) {
                     }
                     mediaPlayer.start();
-                    calculateScaling(bot);
+                    mediaController.setMediaPlayer(SRBeastModeVideoView.this);
+                    mediaController.setAnchorView(SRBeastModeVideoView.this);
+                    mediaController.setEnabled(true);
+                    mediaController.show();
+                    calculateScaling(1920);
                 }
             });
+
             mediaPlayer.prepareAsync();
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        } catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
+        } catch (IllegalArgumentException | IOException | IllegalStateException | SecurityException e) {
             e.printStackTrace();
         }
     }
-
 
     @Override
     public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
@@ -92,6 +101,8 @@ public class SRBeastModeVideoView extends TextureView implements TextureView.Sur
 
     @Override
     public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+        mediaPlayer.pause();
+        mediaPlayer.stop();
         mediaPlayer.release();
         return false;
     }
@@ -113,6 +124,16 @@ public class SRBeastModeVideoView extends TextureView implements TextureView.Sur
         return perc * percantage;
     }
 
+    public void changeSeekBarColor(int color) {
+        SeekBar seekBar;
+        LinearLayout root;
+        LinearLayout child;
+        root = (LinearLayout) mediaController.getChildAt(0);
+        child = (LinearLayout) root.getChildAt(1);
+        seekBar = (SeekBar) child.getChildAt(1);
+        seekBar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        seekBar.getThumb().setColorFilter(color, PorterDuff.Mode.SRC_IN);
+    }
 
     public void calculateScaling(float i) {
         Matrix matrix = new Matrix();
@@ -129,9 +150,20 @@ public class SRBeastModeVideoView extends TextureView implements TextureView.Sur
         return mediaPlayer;
     }
 
-    public void initializeBeastMode() {
+    public void initializeBeastMode(Uri videoUri) {
+        video = videoUri;
         if (mediaPlayer == null) {
             mediaPlayer = new MediaPlayer();
+            mediaController = new MediaController(getContext());
+            setSurfaceTextureListener(this);
+        }
+    }
+
+    public void initializeBeastMode(String videoPath) {
+        path = videoPath;
+        if (mediaPlayer == null) {
+            mediaPlayer = new MediaPlayer();
+            mediaController = new MediaController(getContext());
             setSurfaceTextureListener(this);
         }
     }
@@ -151,6 +183,57 @@ public class SRBeastModeVideoView extends TextureView implements TextureView.Sur
 
     public void pause() {
         mediaPlayer.pause();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mediaController.show();
+        return false;
+    }
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int i) {
+        mediaPlayer.seekTo(i);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
     }
 }
 
